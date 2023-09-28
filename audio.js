ccefd145c2fb8da999dede3fbc98f9677a9e76b2
olga <olga.rudacivs1991@gmail.com>
2023-09-28 12:02:17 +0500

    js

--- audio.js
+++ audio.js
@@ -1,1 +1,18 @@
+/*main*/
+const audio = document.querySelector('audio');
+const button = document.querySelector('button');
+const playNum = 0;
 
+function isPlay() {
+    if(audio.paused == true)
+    {audio.play();}
+    else if (audio.paused == false)
+{audio.pause();}
+}
+
+function toggleBtn() {
+    button.classList.toggle('pause');
+  }
+  button.addEventListener('click', toggleBtn);
+
+/* pic */
